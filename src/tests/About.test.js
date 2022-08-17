a97a17219c9bb33e1a01e7109f0c97ad83b58fbd
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-08-17 13:24:37 -0300

    Requisito 3

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -20,7 +20,6 @@ describe('Requisito 2 - Teste o componente About.js', () => {
     const aboutSecondInfo = screen.getByText(/One can filter Pokémons by type/i);
     expect(aboutSecondInfo).toBeInTheDocument();
 
-    // src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png'
     const img = screen.getByRole('img', { name: 'Pokédex' });
     expect(img).toBeInTheDocument();
     expect(img).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png');
--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,37 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+import { FavoritePokemons } from '../pages';
+import data from '../data';
+
+describe('Requisito 3 - Teste o componente FavoritePokemons.js', () => {
+  test('Se nenhum pokémon for encontrado, exiba "No favorite pokemon found"', () => {
+    const { history } = renderWithRouter(<App />);
+    const checkFavorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    userEvent.click(checkFavorites);
+    expect(history.location.pathname).toBe('/favorites');
+
+    const favoriteTitle = screen.getByRole('heading', {
+      level: 2, name: 'Favorite pokémons' });
+    expect(favoriteTitle).toBeInTheDocument();
+
+    const notFound = screen.getByText('No favorite pokemon found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  test('Testa se exibe todos os pokémons favoritos', () => {
+    renderWithRouter(<FavoritePokemons pokemons={ data } />);
+
+    const favoriteTitle = screen.getByRole('heading', {
+      level: 2, name: 'Favorite pokémons' });
+    expect(favoriteTitle).toBeInTheDocument();
+
+    const favoritesPokemons = screen.getAllByTestId('pokemon-name');
+    expect(favoritesPokemons[3]).toHaveTextContent(/Ekans/i);
+    expect(favoritesPokemons[8]).toHaveTextContent(/Dragonair/i);
+    expect(favoritesPokemons[7]).toHaveTextContent(/Snorlax/i);
+    expect(favoritesPokemons[5]).toHaveTextContent(/Mew/i);
+  });
+});
