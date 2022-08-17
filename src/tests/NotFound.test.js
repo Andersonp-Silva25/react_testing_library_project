fd2d80e93b48794fba910e17442424a52cd54d1d
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2022-08-17 14:45:23 -0300

    Requisito 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Requisito 4 - Teste o componente NotFound.js', () => {
+  test('Testa se a página contem um h2 "Page requested not found"', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/achouNão');
+
+    const notFoundTitle = screen.getByRole('heading',
+      { level: 2, name: /Page requested not found/i });
+
+    expect(notFoundTitle).toBeInTheDocument();
+  });
+
+  test('Testa se a página exibe a imagem corretamente', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/achouNão');
+
+    const img = screen.getByRole('img',
+      { name: 'Pikachu crying because the page requested was not found' });
+    expect(img).toBeInTheDocument();
+    expect(img).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
